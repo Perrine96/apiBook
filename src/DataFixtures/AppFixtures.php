98b05ed7197e36a47bb1b5973108ec04a3815150
Perrine96 <perrinemallard96@gmail.com>
2025-08-21 14:08:33 +0200

    user + validation données + jwt

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,11 +6,34 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Book;
 use App\Entity\Author;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
+
+        // Création d'un user normal
+        $user = new User();
+        $user->setEmail('user@bookapi.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        // Création d'un user admin
+        $user = new User();
+        $user->setEmail('admin@bookapi.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
         // Création des auteurs
         $listAuthor = [];
         for ($i = 0; $i < 10; $i++) {
